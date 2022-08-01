f68f9dcbc904955ee63c0f90eb97f776142e3b5a
Bofoa precious <bofoaprecious@mail.com>
2022-08-01 20:12:11 +0100

    added console.log

---
+++ java script/script.js
@@ -0,0 +1,25 @@
+let Height1 = 1.69;
+let mass1 = 78
+let Height3 = 1.95
+let mass3 = 92
+let Height2 = 1.88
+let mass2 = 95
+let Height4 = 1.76
+let mass4 = 85
+
+function johnBodyMass1(){
+    let BMI=mass1/height1*height1
+    console.long(BMI)
+}
+function johnBodyMass2(){
+    let BMI=mass2/heiht2*height2
+    console.log(BMI)
+}
+function markBodyMass1(){
+    let BMI=mass3/height3*height3
+    console.log(BMI)
+}
+function markBodyMass2(){
+    let BMI=mass4/height4*height4
+    console.log(BMI)
+}
