16c8b4f6c47bc4b20143f2abcaf1edf45f3448a5
Bofoa precious <bofoaprecious@mail.com>
2022-08-04 21:11:46 +0100

    added function

--- java script/script.js
+++ java script/script.js
@@ -1,25 +1,56 @@
-let Height1 = 1.69;
-let mass1 = 78
-let Height3 = 1.95
-let mass3 = 92
-let Height2 = 1.88
-let mass2 = 95
-let Height4 = 1.76
-let mass4 = 85
-
+let johnHeight1 = 1.95
+let markHeight2 = 1.88
+let markMass1 = 78
+let johnMass2 = 85 
+let markMass2 = 95
+let johnMass1 = 92
+let johnHeight2 =1.76
+let markHeight1 = 1.69
+let bmi = johnMass1/johnHeight1*johnHeight1**2
+let bmi2 = johnMass2/johnHeight2*johnHeight2**2
+let bmi3 = markMass1/markHeight1*markHeight1**2
+let bmi4 = markMass2/markHeight2*markHeight2**2 
+const best = 179.4
+const bestNew = 'john has a higher body index ${best}'
+const johnBodyMassIndex = 179.4
+const highestBodyMass = johnBodyMassIndex >= 179.4;
+console.log(highestBodyMass);
+if (highestBodyMass){
+    console.log(bestNew);
+}else {
+    const markBodyMassIndex = 147.5;
+    console.log("mark has a lesser body index");
+}
+//let hbm = bmi
 function johnBodyMass1(){
-    let BMI=mass1/height1*height1
-    console.long(BMI)
+    let johnBodyMass1= bmi
+    console.log(johnBodyMass1)
 }
-function johnBodyMass2(){
-    let BMI=mass2/heiht2*height2
-    console.log(BMI)
+johnBodyMass1()
+function johnBodyMass2() {
+    let johnBodyMass2 = bmi2
+    console.log(johnBodyMass2)
+    
 }
-function markBodyMass1(){
-    let BMI=mass3/height3*height3
-    console.log(BMI)
+johnBodyMass2()
+function markBodyMass1() {
+    let markBodyMass1= bmi3
+    console.log(markBodyMass1)
+    
 }
+markBodyMass1()
 function markBodyMass2(){
-    let BMI=mass4/height4*height4
-    console.log(BMI)
+    let markBodyMass2= bmi4
+    console.log(markBodyMass2)
 }
+//markBodyMass2()
+//function markBodyMass() {
+  //  let markBodyMass = hbm
+    //console.log(markBodyMass)
+    
+//}
+//function highestBodyMass(){
+  //  let highestBodyMass = hbm
+    //console.log(highestBodyMass)
+//}
+//highestBodyMass()
