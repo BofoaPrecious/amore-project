16010b441a7b541ba820e4f74454c93d54a7a0a0
Bofoa precious <bofoaprecious@mail.com>
2022-08-12 18:42:47 +0100

    added ternary

---
+++ java script/js lession 3/script.js
@@ -0,0 +1,8 @@
+let billValue = (275,40,430)
+console.log(billValue)
+const tip= billValue*20/100
+tip <50>300? console.log("tip is 15%"): console.log("tip is 20%")
+console.log(tip)
+const totalValue=billValue+tip
+console.log(totalValue)
+
